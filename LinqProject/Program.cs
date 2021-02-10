c3b6f09b8a713a1ad40a21cd94bf39f163b63203
Ceren <Zcerens@users.noreply.github.com>
2021-02-11 02:23:25 +0300

    .1

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -71,7 +71,7 @@ namespace LinqProject
 
     class Product
     {
-        public int ProductId { get; set; }  
+        public int ProductId { get; set; }
         public int CategoryId { get; set; } 
         public string ProductName { get; set; }
         public string QuantitiyPerUnit { get; set; }
