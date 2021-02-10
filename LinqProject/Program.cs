cb06f38688822effa9d3c9fa0be61418ef038b10
Ceren <Zcerens@users.noreply.github.com>
2021-02-10 23:04:18 +0300

    Update 10.02

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -8,7 +8,7 @@ namespace LinqProject
     {
         static void Main(string[] args)
         {
-            List<Category> categories = new List<Category>
+            List<Category> categories = new List<Category> 
             {
                 new Category{CategoryId=1, CategoryName="Bilgisayar"},
                 new Category{CategoryId=2, CategoryName="Telefon"},
@@ -74,6 +74,7 @@ namespace LinqProject
         public string QuantitiyPerUnit { get; set; }
         public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
+
     }
 
     class Category
